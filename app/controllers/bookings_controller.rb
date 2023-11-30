b3cc2639ba9718a15c217a6748334fa23c42ac66
Joey <gibbonsjoey95@gmail.com>
2023-11-29 22:14:23 -0600

    Did part of part 3 number 5

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
     def new
+        @selected_flight = Flight.find(params[:selected_flight_id])
+        @num_passengers = params[:passenger_count].to_i
     end
 end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,26 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @flights = Flight.all
-
     @flight_dates = Flight.pluck(:start_datetime).map {|datetime| datetime.to_date}.uniq.sort
-    
-    @flights = @flights.where(departure_airport_id: params[:departure_airport]) if params[:departure_airport].present?
-    @flights = @flights.where(arrival_airport_id: params[:arrival_airport]) if params[:arrival_airport].present?
-    @flights = @flights.where("DATE(start_datetime) = ?", params[:flight_date]) if params[:flight_date].present?
-    @flights = @flights.order(start_datetime: :asc)
+
+    if params[:commit] == 'Search Flights' && valid_search_params?
+      @flights = Flight.all
+
+      @flights = @flights.where(departure_airport_id: params[:departure_airport]) if params[:departure_airport].present?
+      @flights = @flights.where(arrival_airport_id: params[:arrival_airport]) if params[:arrival_airport].present?
+      @flights = @flights.where("DATE(start_datetime) = ?", params[:flight_date]) if params[:flight_date].present?
+      @flights = @flights.order(start_datetime: :asc)
+    else
+      @flights = []
+    end
+
+    @passenger_count = params[:passenger_count]
+  end
+
+  private
+  
+  def valid_search_params?
+    %i[departure_airport arrival_airport flight_date].any? {|param| params[param].present?} && params[:passenger_count].to_i.positive?
   end
 end
 
